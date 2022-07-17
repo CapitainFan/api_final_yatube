f04c17d545820b14e5649679123d7451ae33be8d
CapitainFan <gudokrulit@mail.ru>
2022-07-17 16:12:54 +0300

    pytest

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # api_final
-api final
+### Описание
+Это тестовый сервер для работы с API
+### Технологии
+Python 3.7 ;  
+Django 2.2.19 ;  
+API  
+### Как запустить проект:
+
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git@github.com:CapitainFan/api_final_yatube.git
+```
+
+```
+cd api_final_yatube
+```
+
+Cоздать и активировать виртуальное окружение:
+
+```
+python3 -m venv env
+```
+
+```
+source env/bin/activate
+```
+
+```
+python3 -m pip install --upgrade pip
+```
+
+Установить зависимости из файла requirements.txt:
+
+```
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+
+```
+cd yatube_api
+```
+
+```
+python3 manage.py migrate
+```
+
+Запустить проект:
+
+```
+python3 manage.py runserver
+```
+
+### Авторы
+Никита Гудков
+
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,44 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from posts.models import Group, Post
+from .serializers import (CommentSerializer, GroupSerializer, PostSerializer,
+                          FollowSerializer)
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+from .mypaginations import MyLimitOffsetPagination
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    pagination_class = MyLimitOffsetPagination
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticated)
+    pagination_class = MyLimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticated)
+    pagination_class = MyLimitOffsetPagination
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated, )
+    pagination_class = MyLimitOffsetPagination
